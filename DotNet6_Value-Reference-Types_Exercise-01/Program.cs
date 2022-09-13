6eaaad6aa3fafbcfc4f74274643fe6c6b4738190
John Ward <johndward01@gmail.com>
2022-09-13 16:42:25 -0500

    update

--- DotNet6_Value-Reference-Types_Exercise-01/Program.cs
+++ DotNet6_Value-Reference-Types_Exercise-01/Program.cs
@@ -2,56 +2,73 @@
 using DotNet6_Value_Reference_Types_Exercise_01;
 using System.Text;
 
-// TODO: Use the 3 value type variables that are already declared and initialized below
 double myDouble = .005;
 int myInteger = 10;
 string myString = "Hello World";
 
+Console.WriteLine("-------------VALUE TYPES-----------------");
+
 // TODO: Write out each of these to the Conosle like so:
-// Console.WriteLine($"Before: {yourVariable}");
+// Console.WriteLine($"Before: {yourVarible}");
 
 // TODO: Use the ChangeValueType() function to "change the value type" for each variable
-// ChangeValueType(yourVariable);
+// Methods.ChangeValueType(yourVariable);
 
-Methods.AddSpaces(3);
+Methods.AddSpaces(1);
 
 // Thought Experiment: What will the value of the original variable be after going through the ChangeValueType() method?
-// TODO: Double variable value = ?
-// TODO: Integer variable value = ?
-// TODO: String variable value = ?
+// TODO: Double variable value = .005
+// TODO: Integer variable value = 10
+// TODO: String variable value = "Hello World"
 
 // Why:
-// Answer: ?
+// Answer: Because these are Value Types their values are copied, therefore, when you write the original to the conosle none of them have changed!
 
-// TODO: Write out the variable to the console after being passed through the ChangeValueType() function
-// Console.WriteLine($"Adjusted: {yourVariable}");
+// TODO: Write out the original variable to the console after being changed by the ChangeValueType() function
+// Console.WriteLine($"Original: {yourVariable}");
 
 
-Methods.AddSpaces(3);
+Methods.AddSpaces(1);
 
 
 // TODO: Use the 4 reference type variables that are already declared and initialized below
 StringBuilder sb = new StringBuilder("The starting string's value.");
 List<int> myList = new List<int>() { 1, 2, 3 };
 int[] myArray = new int[] { 4, 5, 6 };
-Dog myDogObject = new Dog { Name = "Lassy", Breed = "Rough Collie" };
+Dog myDogObject = new Dog { Name = "Lassy" };
 
-// TODO: Write out each of these to the Conosle. Remember to use a loop for the list and array!
+Console.WriteLine("-------------REFERENCE TYPES-----------------");
 
+// TODO: Write out each of these to the Conosle. Remember to use a loop for the list and array
 
-// TODO: Use the ChangeReferenceType() function to "change the reference" for each variable
+Methods.AddSpaces(1);
+
+Methods.AddSpaces(1);
+
+Methods.AddSpaces(1);
+
+Methods.AddSpaces(1);
+
+// TODO: Use the ChangeReferenceType() function to "change the reference type" for each variable
 
 // Thought Experiment: What will the value of the original variable be after going through the ChangeReferenceType() method?
-// TODO: StringBuilder variable value = ?
-// TODO: List<int> variable values = ?
-// TODO: int[] variable values = ?
-// TODO: Dog object variable's name = ?
+// TODO: StringBuilder variable value = The ending string's value.
+// TODO: List<int> variable values = 100200300
+// TODO: int[] variable values = 400500600
+// TODO: Dog object variable's name = A changed name
 
 // Why:
-// Answer: ?
+// Answer: Because these are Reference Types their values are not copied, therefore, when you write the original to the conosle they will be changed!
 
 
-// TODO: Write out the adjusted variables to the console after being passed through the ChangeReferenceType() function
+// TODO: Write out the original variables to the console after being changed by the ChangeReferenceType() function
+
+Methods.AddSpaces(1);
+
+Methods.AddSpaces(1);
+
+Methods.AddSpaces(1);
+
 
 
 
